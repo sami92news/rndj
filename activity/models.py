582cb2354b4434cf318425f5bf5f41f9ed426945
humblesami <Prvmgo123@45>
2021-02-05 01:06:31 +0500

    model ordering

--- activity/models.py
+++ activity/models.py
@@ -2,6 +2,8 @@ from django.db import models
 
 
 class Student(models.Model):
+    class Meta:
+        ordering = ('-id',)
     student_name = models.CharField(max_length=255, null=True)
     student_phone_number = models.CharField(max_length=255, null=True)
     student_email = models.CharField(max_length=255, null=True)
